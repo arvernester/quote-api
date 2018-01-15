99497a526ba70dc21506c37e02b6330ec6c93fb6
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-01-15 17:31:36 +0700

    Change table to view to card mode

--- resources/assets/js/components/quote/Index.vue
+++ resources/assets/js/components/quote/Index.vue
@@ -1,28 +1,15 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
-        <div class="panel panel-default">
-          <div class="panel-heading">Quotes ({{ quotes.total }})</div>
-          <div class="panel-body">
-            <div class="table-responsive">
-              <table class="table">
-                <thead>
-                  <tr>
-                    <th>Category</th>
-                    <th>Quote</th>
-                    <th>Author</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="(quote, index) in quotes.data">
-                    <td>{{ quote.category.name }}</td>
-                    <td>{{ quote.text }}</td>
-                    <td>{{ quote.author }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+      <div class="col-md-6 col-md-offset-3">
+        <div v-for="(quote, index) in quotes.data" class="card">
+
+          <div class="card-content">
+            <blockquote>
+              <p>{{ quote.text }}</p>
+
+            </blockquote>
+              <p class="author"><strong>{{ quote.author }}</strong></p>
           </div>
         </div>
 
@@ -64,3 +51,76 @@
     }
   }
 </script>
+
+<style scoped>
+  .card {
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  }
+
+  .card {
+    margin-top: 10px;
+    box-sizing: border-box;
+    border-radius: 2px;
+    background-clip: padding-box;
+  }
+
+  .card span.card-title {
+    color: #fff;
+    font-size: 24px;
+    font-weight: 300;
+    text-transform: uppercase;
+  }
+
+  .card .card-image {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .card .card-image img {
+    border-radius: 2px 2px 0 0;
+    background-clip: padding-box;
+    position: relative;
+    z-index: -1;
+  }
+
+  .card .card-image span.card-title {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: 16px;
+  }
+
+  .card .card-content {
+    padding: 16px;
+    border-radius: 0 0 2px 2px;
+    background-clip: padding-box;
+    box-sizing: border-box;
+    background-color: #fff;
+  }
+
+  .card .card-content p {
+    margin: 0;
+    color: inherit;
+  }
+
+  .card .card-content span.card-title {
+    line-height: 48px;
+  }
+
+  .card .card-action {
+    border-top: 1px solid rgba(160, 160, 160, 0.2);
+    padding: 16px;
+  }
+
+  .card .card-action a {
+    color: #ffab40;
+    margin-right: 16px;
+    transition: color 0.3s ease;
+    text-transform: uppercase;
+  }
+
+  .card .card-action a:hover {
+    color: #ffd8a6;
+    text-decoration: none;
+  }
+</style>
