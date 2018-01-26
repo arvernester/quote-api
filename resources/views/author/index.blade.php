9e72b8da7ef36b7ef7ee5645f53d2ab041e26faa
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-01-26 20:35:17 +0700

    Wrap as responsive table

--- resources/views/author/index.blade.php
+++ resources/views/author/index.blade.php
@@ -1,46 +1,48 @@
 @extends('layouts.admin')
 
 @section('content')
+
 @include('layouts.partials.flash')
 
 <div class="row">
     <div class="col-md-12">
         <div class="panel panel-default">
-            <div class="panel-heading">Famous authors around the world</div>
             <div class="panel-body">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Author Name</th>
-                            <th>Has Picture</th>
-                            <th>Created</th>
-                            <th>Updated</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Author Name</th>
+                                <th>Has Picture</th>
+                                <th>Created</th>
+                                <th>Updated</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
 
-                    <tbody>
-                        @foreach ($authors as $author)
-                        <tr>
-                            <td>{{ $author->name }}</td>
-                            <td>{{ $author->image_path ? 'Yes' : 'No' }}</td>
-                            <td>{{ $author->created_at }}</td>
-                            <td>{{ $author->updated_at }}</td>
-                            <td>
-                                <a href="{{ route('admin.author.show', $author) }}" class="btn btn-primary btn-table">
-                                    <i class="fa fa-eye fa-fw"></i>
-                                </a>
-                                <a href="{{ route('admin.author.edit', $author) }}" class="btn btn-primary btn-table">
-                                    <i class="fa fa-edit fa-fw"></i>
-                                </a>
-                                <a href="{{ route('admin.author.edit', $author) }}" class="btn btn-danger btn-table delete">
-                                    <i class="fa fa-trash fa-fw"></i>
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        <tbody>
+                            @foreach ($authors as $author)
+                            <tr>
+                                <td>{{ $author->name }}</td>
+                                <td>{{ $author->image_path ? 'Yes' : 'No' }}</td>
+                                <td>{{ $author->created_at }}</td>
+                                <td>{{ $author->updated_at }}</td>
+                                <td>
+                                    <a href="{{ route('admin.author.show', $author) }}" class="btn btn-primary btn-table">
+                                        <i class="fa fa-eye fa-fw"></i>
+                                    </a>
+                                    <a href="{{ route('admin.author.edit', $author) }}" class="btn btn-primary btn-table">
+                                        <i class="fa fa-edit fa-fw"></i>
+                                    </a>
+                                    <a href="{{ route('admin.author.edit', $author) }}" class="btn btn-danger btn-table delete">
+                                        <i class="fa fa-trash fa-fw"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
 
