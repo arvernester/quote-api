db3eb107fa98cc3f15c7f9f530961786f887f816
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-01-27 17:33:36 +0700

    Fixed first parameter

--- app/Quote.php
+++ app/Quote.php
@@ -40,7 +40,7 @@ class Quote extends Model
      */
     public function setTextAttribute(string $text)
     {
-        if (!ends_with('aku', ['.', '...', '?', '!'])) {
+        if (!ends_with($text, ['.', '...', '?', '!'])) {
             $text .= '.';
         }
 
