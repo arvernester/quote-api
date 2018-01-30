e6b3caa795ca1dabb4575f18ab2ad85e50a66cc2
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-01-30 13:45:42 +0700

    Change footer name

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -177,7 +177,7 @@
 		<!-- Copyright -->
 		<div id="copyright">
 			<ul>
-				<li>&copy; Untitled</li>
+				<li>&copy; {{ config('app.name') }}</li>
 				<li>Design:
 					<a href="https://html5up.net">HTML5 UP</a>
 				</li>
