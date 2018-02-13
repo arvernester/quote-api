f65d790ce3bdeefb80b5345237fc303905a848ba
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-02-14 01:04:03 +0700

    Translations

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,24 +2,24 @@
 
 @section('content')
 <div class="login-panel panel panel-default">
-    <div class="panel-heading">Log in</div>
+    <div class="panel-heading">{{ __('Login') }}</div>
     <div class="panel-body">
         <form method="post" action="{{ route('login') }}" role="form">
             {{ csrf_field() }}
 
             <fieldset>
                 <div class="form-group {{ !$errors->has('email') ?: 'has-error' }}">
-                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus="" value="{{ old('email') }}">
+                    <input class="form-control" placeholder="{{ __('Email') }}" name="email" type="email" autofocus="" value="{{ old('email') }}">
                 </div>
                 <div class="form-group {{ !$errors->has('password') ?: 'has-error' }}">
-                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                    <input class="form-control" placeholder="{{ __('Password') }}" name="password" type="password" value="">
                 </div>
                 <div class="checkbox">
                     <label>
-                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                        <input name="remember" type="checkbox" value="Remember Me">{{ __('Remember Me') }}
                     </label>
                 </div>
-                <button class="btn btn-primary" type="submit">Login</button>
+                <button class="btn btn-primary" type="submit">{{ __('Log In') }}</button>
 
             </fieldset>
         </form>
