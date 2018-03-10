fd7068b745e41c2ff6516afe045f0f6ce0766da1
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-03-10 19:43:39 +0700

    Import quote from new vendor

---
+++ app/Quote/FavqsQuote.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Quote;
+
+use App\Contracts\Quote;
+use Unirest\Request;
+use Illuminate\Support\Facades\Log;
+
+class FavqsQuote implements Quote
+{
+    /**
+     * Import quote of the day from https://favqs.com/api/qotd.
+     *
+     * @return array|null
+     */
+    public function import(): ? array
+    {
+        $request = Request::get($url = 'https://favqs.com/api/qotd');
+
+        if ($request->code == 200) {
+            $quote = $request->body->quote;
+
+            $category = 'Uncategorized';
+            if (!empty($quote->tags)) {
+                $category = ucwords(collect($quote->tags)->first());
+            }
+
+            return [
+                'author' => $quote->author,
+                'quote' => $quote->body,
+                'category' => $category,
+                'language' => 'en',
+                'source' => $quote->url,
+            ];
+        }
+
+        Log::error(sprintf('Failed to get response from %s.', $url), [
+            'code' => $response->code,
+        ]);
+
+        return null;
+    }
+}
