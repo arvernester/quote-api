cf84daf6dabeb96bfd38bb98f8091c33ebb273c7
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-03-10 21:17:15 +0700

    Import quote from Forbes

---
+++ app/Quote/ForbesQuote.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Quote;
+
+use App\Contracts\Quote;
+use Unirest\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\File;
+
+class ForbesQuote implements Quote
+{
+    public function import(): ? array
+    {
+        $dir = storage_path('quotes/');
+        if (!File::isDirectory($dir)) {
+            File::makeDirectory($dir, 0755, true);
+        }
+
+        $path = $dir.'forbes.txt';
+
+        if (File::exists($path)) {
+            $forbes = json_decode(File::get($path));
+            $query = $forbes->query;
+        } else {
+            $query = 1;
+        }
+
+        $request = Request::get($url = 'https://www.forbes.com/forbesapi/thought/uri.json?enrich=false&query='.$query);
+
+        if ($request->code == 200) {
+            $quote = $request->body->thought;
+
+            // increment start value when request is succeed
+            File::put($path, json_encode([
+                'query' => $query + 1,
+            ]));
+
+            if (!empty($quote)) {
+                $category = 'Uncategorized';
+                if (!empty($quote->thoughtThemes)) {
+                    $category = ucwords(collect($quote->thoughtThemes)->first()->name);
+                }
+
+                return [
+                    'language' => 'en',
+                    'author' => $quote->thoughtAuthor->name,
+                    'quote' => $quote->quote,
+                    'category' => $category,
+                    'source' => $quote->shortUri,
+                ];
+            }
+
+            Log::warning(sprintf('Empty quote from Forbes (%s).', $url));
+        }
+
+        Log::error(sprintf('Failed to get response from %s.', $url), [
+            'code' => $request->code,
+        ]);
+
+        return null;
+    }
+}
